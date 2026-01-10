3e11cadf7518512dbebf1f07240aef7c140bffa6
Lobotomy2012 <luongnaman0906.amongus@gmail.com>
2026-01-10 15:33:57 +0700

    idk

--- scripts/register.js
+++ scripts/register.js
@@ -3,7 +3,8 @@ const email = document.getElementById('email')
 const password = document.getElementById('password')
 const signup_form = document.getElementById('sign_up')
 
-signup_form.addEventListener('submit', function() {
+signup_form.addEventListener('submit', function(e) {
+    e.preventDefault()
     if (username.value === '' || email.value === '' || password.value === '') {
         alert('Please fill in all fields.')
         return false
